d9cb2a6864306a4712774fb49f58a1001af89bd2
Arthur Vincent Simon <developarvin@gmail.com>
2017-01-17 11:34:15 +0800

    - Implement state persistence using redux-persist - Limit the number of stories to get from API

--- app/config/api.js
+++ app/config/api.js
@@ -1,4 +1,4 @@
 export const hackerNewsApiEndpoints = {
     newStories: 'https://hacker-news.firebaseio.com/v0/newstories',
-    frontPageStories: 'http://hn.algolia.com/api/v1/search?tags=front_page'
+    frontPageStories: 'http://hn.algolia.com/api/v1/search?tags=front_page&hitsPerPage=8'
 }
--- app/config/store.js
+++ app/config/store.js
@@ -1,11 +1,16 @@
+import { AsyncStorage } from 'react-native'
 import { createStore, applyMiddleware  } from 'redux';
 import thunk from 'redux-thunk'
+import { persistStore, autoRehydrate } from 'redux-persist'
+
 import rootReducer from '../reducers/index.js'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 const store = createStoreWithMiddleware(
     rootReducer,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk),
+    autoRehydrate()
 );
+persistStore(store, { storage: AsyncStorage })
 
 export default store;
